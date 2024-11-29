9ed3d3975760d85c6bbf37388447f7b90188dfa0
Tai-cmd <taifazan778@gmail.com>
2024-11-29 14:31:35 +0000

    Criei o pritf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,9 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+    printf("Desafio Super Trunfo -Países!\n");
+   
+   
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
